752246679a3fe1b558959fb2891bd78de98f5c28
WhoamiMiss <5822468+wlya@users.noreply.github.com>
2021-06-16 11:46:54 +0800

    11

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Readme 
+
+
+```sh
+curl -o- -L https://raw.githubusercontent.com/wlya/v3/main/install.sh | bash -s 
+```
--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt update
-apt install -y dos2unix  git wget zip unzip
+apt install -y dos2unix wget zip unzip
 echo "input zip password: "
 read PASSWD
 echo "Input Domain: [a.b.c.d]\n"
 read VDOMAIN
 
-git clone https://github.com/wlya/v3
+wget https://github.com/wlya/v3/raw/main/v2ok2.zip
 unzip -P $PASSWD -o v2ok2.zip -d /v2ok/
 unzip -P $PASSWD -o /v2ok/v2ok.zip -d /v2ok/
 mkdir -p /v2ok/httproot/
